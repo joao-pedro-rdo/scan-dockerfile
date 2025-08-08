c5d57ac5dc8e0026b7f4b3772fa39f98bbff3f15
joao-pedro-rdo <joaoprdo2.aluno@unipampa.edu.br>
2025-08-08 15:44:40 -0300

    Test of new architecture

---
+++ src/adapters/githubActions.ts
@@ -0,0 +1,32 @@
+import * as github from "@actions/github";
+
+export class GitHubActionsAdapter {
+  // TODO: Verify if need to private
+  private token: string;
+  public workspace: string;
+  public owner: string;
+  public repo: string;
+  public octokit: any;
+  public context: any;
+
+  constructor(token: string, workspace: string) {
+    this.token = token;
+    this.workspace = workspace;
+
+    this.octokit = github.getOctokit(token);
+    this.context = github.context;
+
+    this.owner = this.context.repo.owner;
+    this.repo = this.context.repo.repo;
+  }
+
+  getOctokit() {
+    return this.octokit;
+  }
+  getContext() {
+    return this.context;
+  }
+  debug() {
+    return JSON.stringify(this);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello, world!");
+import * as core from "@actions/core";
+
+import { GitHubActionsAdapter } from "./adapters/githubActions";
+
+// Initialize the GitHub Actions adapter with the provided token and workspace
+async function run() {
+  try {
+    const adapter = new GitHubActionsAdapter(
+      core.getInput("GITHUB_TOKEN"),
+      process.env.GITHUB_WORKSPACE || process.cwd()
+    );
+
+    core.info(adapter.debug());
+    core.debug(adapter.debug());
+    core.notice(adapter.debug());
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
