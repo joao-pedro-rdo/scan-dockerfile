90e914695d246ad2086e64e65e9b869d15569a22
joao-pedro-rdo <joaoprdo2.aluno@unipampa.edu.br>
2025-08-29 15:34:20 -0300

    test of ISSUE and PR

--- src/index.ts
+++ src/index.ts
@@ -27,11 +27,14 @@ async function run() {
 
     console.log(adapter.debug());
 
+    console.log("test of new issue");
+    console.log("something");
     reporter.newIssue({
       title: "New Issue Title",
       body: "Description of the new issue",
       labels: ["dockerfile", "scan-dockerfile"],
     });
+    console.log("teste of new PR");
     reporter.newPr({
       title: "New Pull Request Title",
       body: "Description of the new pull request",
